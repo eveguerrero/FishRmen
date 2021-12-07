2e7c8999b90331e82b11152c4ba993e57045ae29
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-07 13:04:16 -0500

    Initialize

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-fetch('https://www.fishwatch.gov/developers')
-.then(response => response.json())
-    .then(data => console.log(data));
+
+    fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+            "x-rapidapi-key": "171e674b90msh9a40f27ddb32a16p15e6bdjsnfe86444363cc"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+
+
+
+
+
+
+// const projectURL = 'the-cocktail-db.p.rapidapi.com';
+// function getMeme(){
+//     fetch(projectURL)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(data => console.log(data));
+// }
+
+
+
+
+
+   
+
+
     
