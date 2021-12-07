cbce6c8eac0b0e1cc0d63d198b0d6dd9a5534cfd
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-07 13:37:32 -0500

    Initialize project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# getMeme-
-# getMeme-
+# fishRmen-
+# fishRmen-
 
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+fetch('https://www.fishwatch.gov/api/species') 	
+.then(response => response.json())
+.then(data => console.log(data));
 
-    fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-            "x-rapidapi-key": "171e674b90msh9a40f27ddb32a16p15e6bdjsnfe86444363cc"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+
+
+
+
+
+
+    // fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+    //         "x-rapidapi-key": "171e674b90msh9a40f27ddb32a16p15e6bdjsnfe86444363cc"
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // });
     
 
 
@@ -19,15 +28,7 @@
 
 
 
-// const projectURL = 'the-cocktail-db.p.rapidapi.com';
-// function getMeme(){
-//     fetch(projectURL)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(data => console.log(data));
-// }
-
+ 
 
 
 
