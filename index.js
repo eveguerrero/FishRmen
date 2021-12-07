b7ac656c9890794938a2bd53adaa89f36f3bb02e
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-07 14:11:38 -0800

    Adding JS submit event

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-fetch('https://www.fishwatch.gov/api/species') 	
-.then(response => response.json())
-.then(data => console.log(data));
 
-
-
-
-
-
-
-    // fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin", {
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-    //         "x-rapidapi-key": "171e674b90msh9a40f27ddb32a16p15e6bdjsnfe86444363cc"
-    //     }
-    // })
-    // .then(response => {
-    //     console.log(response);
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    // });
+const fishWatch = "https://ecos.fws.gov/ecp/pullreports/catalog/species/report/species/export"
     
+    const getFish = () => { 
+    fetch (fishWatch)
+    .then(res => res.json())
+    .then(data => (displayFish(data)));
+    }
+
+    const form = document.getElementById('fish-review');
+    
+  form.addEventListener('submit', (e) => {
+   e.preventDefault();
+    
+           const fishObject = {}
+           fishObject.name = document.getElementById('new-name').value;
+           fishObject.water = document.getElementById('new-water').value;
+           fishObject.image = document.getElementById('new-image').value;
+           fishObject.rating = document.getElementById('new-rating').value;
+           fishObject.comment = document.getElementById('new-comment').value;
+
+           displayFish(fishObject)
+       })
+
+       function displayFish(fish) {
+
+         const fishDisplay = document.getElementById('fish-display');
+
+         const image = document.createElement('img');
+           image.src = fish.src;
+  
+           image.addEventListener('click', () => {
+               document.querySelector('.detail-image').src = fish.image;
+               document.querySelector('.name').innerHTML = fish.name;
+               document.querySelector('.water').innerHTML = fish.water;
+               document.querySelector('#rating-display').innerHTML = fish.rating;
+               document.querySelector('#comment-display').innerHTML = fish.comment;
+
+           }) 
+           fishDisplay.appendChild(image);
+         }
+
+         function displayFishes(fishes) {
+             fishes.forEach(fish => {
+
+                 displayFish(fish);
+             });
+         }
+
+       document.addEventListener('DOMContentLoaded', getFish)
+
+
 
 
 
